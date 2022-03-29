6efa5d4f7f5a9edd289129348078e9cb7ce3c287
xTheM4ST3Rx <them4sterdeveloper@outlook.com>
2022-03-29 14:37:05 -0300

    adcionado controllers

---
+++ controllers/user-controller.js
@@ -0,0 +1,78 @@
+'use strict';
+const User = require("../models/User");
+
+
+exports.getAllUsers = async (req,res,next) => {
+    try {
+        let users = await User.find({});
+        res.send(users)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+exports.getUserById = async (req,res, next) =>{
+    try {
+        const { id } = req.body;
+        let users = await User.findOne({_id: id});
+        res.send(users)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+exports.createUser = async (req,res, next) =>{
+    try {
+        const { name} = req.body;
+    //console.log(req.body)
+    newUser = new User({
+        name: name,
+      })
+      newUser.save();
+      res.status(200).send(newUser);
+
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+
+exports.deleteUser = async (req,res, next) =>{
+    try {
+        let userDel = await User.findOneAndRemove({_id: req.body.id});
+        if(userDel){
+            res.status(200).send(userDel);
+        }else{
+            res.status(500).send({"error":"not found"});
+        }
+    } catch (e) {
+        res.status(500).send(e);
+    }
+}
+
+
+exports.updateUser = async (req,res, next) =>{
+    try {
+        const { newname, id,} = req.body;
+   
+       const filter = { _id: id };
+       const update = { name: newname };
+       
+       // `doc` is the document _before_ `update` was applied
+       let doc = await User.findOneAndUpdate(filter, update);
+       res.status(200).send({});
+   
+       } catch (e) {
+           res.status(500).send(e);
+       }
+}
+
+
+
+
+
+
+
+
+
+
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,87 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
 const authHeader = require("../../../middlewares/auth");
-
-const User = require("../../../models/User");
-
-
-//Procura usuário pelo ID
-router.get("/find", async (req,res) =>{
-    try {
-        const { id } = req.body;
-
-        let users = await User.findOne({_id: id});
-        res.send(users)
-    } catch (e) {
-        console.log(e)
-    }
-})
+const controller = require('../../../controllers/user-controller')
 
 
-//Procura todos os users
-router.get("/findall",async (req,res) =>{
-    try {
-        let users = await User.find({});
-        res.send(users)
-    } catch (e) {
-        console.log(e)
-    }
-})
+router.get("/", controller.getAllUsers)
+router.get("/:id", controller.getUserById)
+router.post("/", controller.createUser)
+router.put("/:id", controller.updateById)
+router.delete("/:id",authHeader, controller.deleteUser)
 
-
-//Cria novo usuário
-router.post("/create",(req,res) =>{
-    try {
-        const { name} = req.body;
-
-    //console.log(req.body)
-    newUser = new User({
-        name: name,
-      })
-
-      newUser.save();
-      res.status(200).send(newUser);
-
-    } catch (e) {
-        res.status(500).send(e);
-    }
-})
-
-
-
-
-//Atualiza novo nome de usuário pelo ID
-router.put("/update",async (req,res) =>{
-    try {
-
-     const { newname, id,} = req.body;
-
-    const filter = { _id: id };
-    const update = { name: newname };
-    
-    // `doc` is the document _before_ `update` was applied
-    let doc = await User.findOneAndUpdate(filter, update);
-    res.status(200).send({});
-
-    } catch (e) {
-        res.status(500).send(e);
-    }
-})
-
-
-
-
-//Deleta usuário pelo ID
-router.delete("/delete", authHeader, async (req,res) =>{
-    try {
-        let userDel = await User.findOneAndRemove({_id: req.body.id});
-    res.status(200).send(userDel);
-
-    } catch (e) {
-        res.status(500).send(e);
-    }
-
-})
 
 
 module.exports = router
